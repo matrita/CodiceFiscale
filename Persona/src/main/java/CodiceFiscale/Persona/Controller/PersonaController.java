d6f319dba6386657cd37be90840c01c9b54d503f
d.stasiuc <stasiuc.denis91@gmail.com>
2024-08-27 14:50:14 +0200

    aggiunto @RequiredArgsConstructor al posto di usare @Autowired

--- Persona/src/main/java/CodiceFiscale/Persona/Controller/PersonaController.java
+++ Persona/src/main/java/CodiceFiscale/Persona/Controller/PersonaController.java
@@ -2,16 +2,16 @@ package CodiceFiscale.Persona.Controller;
 
 import CodiceFiscale.Persona.Model.Persona;
 import CodiceFiscale.Persona.Service.PersonaService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class PersonaController {
 
-    @Autowired
-    private PersonaService personaService;
+    private final PersonaService personaService;
 
     @GetMapping("/api/persona/{codiceFiscale}")
     public Persona getPersonaInfo(@PathVariable String codiceFiscale) {
